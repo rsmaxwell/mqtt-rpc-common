a55c8cb1af786b06181b93e979c2b244b007af28
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-06-24 11:16:29 +0100

    update build scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-set -x
 
-BUILD_DIR=$(pwd)/build
-DIST_DIR=$(pwd)/dist
-INFO_DIR=${BUILD_DIR}/info
+BASEDIR=$(dirname "$0")
+SCRIPT_DIR=$(cd $BASEDIR && pwd)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+BUILD_DIR=${PROJECT_DIR}/app/build
 
-. ${INFO_DIR}/maven.sh
+. ${BUILD_DIR}/buildinfo
 
-ZIPFILE=${DIST_DIR}/${NAME}.zip
 
-mvn --batch-mode deploy:deploy-file \
-	-DgroupId=${GROUPID} \
-	-DartifactId=${ARTIFACTID} \
-	-Dversion=${VERSION} \
-	-Dpackaging=${PACKAGING} \
-	-Dfile=${ZIPFILE} \
-	-DrepositoryId=${REPOSITORYID} \
-	-Durl=${URL}
+cd ${PROJECT_DIR}
+
+./gradlew publish --no-daemon --info \
+    -PrepositoryName=${REPOSITORY} \
+    -PprojectVersion=${VERSION}
