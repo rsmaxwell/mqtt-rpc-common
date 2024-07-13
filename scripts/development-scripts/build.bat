d5c5442a81d1d0629cbc44ab4bc5da86a3ff818f
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-13 22:31:02 +0100

    update build scripts

--- scripts/development-scripts/build.bat
+++ scripts/development-scripts/build.bat
@@ -1,7 +1,37 @@
 @echo off
 
-setlocal
-cd %~dp0\..\..
+set local 
+pushd
 
-echo on
-..\gradlew --warning-mode all :mqtt-rpc-common:build
+set BASEDIR=%~dp0
+set SCRIPT_DIR=%BASEDIR%\..
+set SUBPROJECT_DIR=%SCRIPT_DIR%\..
+set PROJECT_DIR=%SUBPROJECT_DIR%\..
+set BUILD_DIR=%SUBPROJECT_DIR%\build
+
+pushd %SCRIPT_DIR%
+set SCRIPT_DIR=%CD%
+popd
+
+pushd %SUBPROJECT_DIR%
+set SUBPROJECT_DIR=%CD%
+popd
+
+pushd %PROJECT_DIR%
+set PROJECT_DIR=%CD%
+popd
+
+pushd %BUILD_DIR%
+set BUILD_DIR=%CD%
+popd
+
+
+
+call %BUILD_DIR%\buildinfo.bat
+
+cd %SUBPROJECT_DIR%
+
+call %PROJECT_DIR%/gradlew build
+
+
+popd
