af31da4a37cb3fac6d55d4db81d41f510231e03b
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-13 22:15:14 +0100

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,23 +9,9 @@ BUILD_DIR=${SUBPROJECT_DIR}/build
 . ${BUILD_DIR}/buildinfo
 
 
-
-
-tree ${SUBPROJECT_DIR}
-
-
-
-
 cd ${PROJECT_DIR}
 
 
 ${PROJECT_DIR}/gradlew :mqtt-rpc-common:publish \
     -PrepositoryName=${REPOSITORY} \
     -PprojectVersion=${VERSION}
-
-    
-ls -al ${SUBPROJECT_DIR}/build/libs
-
-jar -tf ${SUBPROJECT_DIR}/build/libs/*.jar
-
-
