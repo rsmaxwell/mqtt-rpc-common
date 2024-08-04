4bb9685be12e314fd589638f7682bd70805d0771
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-08-04 09:36:18 +0100

    remove recursive call Response.containsKey

--- src/main/java/com/rsmaxwell/mqtt/rpc/common/Response.java
+++ src/main/java/com/rsmaxwell/mqtt/rpc/common/Response.java
@@ -59,10 +59,6 @@ public class Response extends HashMap<String, Object> {
 		return put("code", message);
 	}
 
-	public boolean containsKey(String key) {
-		return containsKey(key);
-	}
-
 	public String getMessage() throws Exception {
 		return getString("message");
 	}
