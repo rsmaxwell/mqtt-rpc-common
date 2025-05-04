f1d06a9144981287cea63c01de3a60a177082d96
Richard Maxwell <richard@rsmaxwell.co.uk>
2025-05-04 07:52:00 +0100

    Add debug versioninfo

--- src/main/java/com/rsmaxwell/mqtt/rpc/common/buildinfo/AbstractBuildInfo.java
+++ src/main/java/com/rsmaxwell/mqtt/rpc/common/buildinfo/AbstractBuildInfo.java
@@ -1,37 +1,29 @@
 package com.rsmaxwell.mqtt.rpc.common.buildinfo;
 
-import lombok.Getter;
+import lombok.Data;
 
+@Data
 public abstract class AbstractBuildInfo implements IBuildInfo {
 
-	@Getter
 	protected String name;
-
-	@Getter
 	protected String version;
-
-	@Getter
 	protected String buildID;
-
-	@Getter
 	protected String builddate;
-
-	@Getter
 	protected String gitCommit;
-
-	@Getter
 	protected String gitBranch;
-
-	@Getter
 	protected String gitURL;
 
-	public void printAll() throws Exception {
-		System.out.printf("%s\n", name);
-		System.out.printf("    version:   %s\n", version);
-		System.out.printf("    buildID:   %s\n", buildID);
-		System.out.printf("    builddate: %s\n", builddate);
-		System.out.printf("    gitCommit: %s\n", gitCommit);
-		System.out.printf("    gitBranch: %s\n", gitBranch);
-		System.out.printf("    gitURL:    %s\n", gitURL);
+	@Override
+	public String toString() {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(name + "\n");
+		sb.append("    version:   " + version + "\n");
+		sb.append("    buildID:   " + buildID + "\n");
+		sb.append("    builddate: " + builddate + "\n");
+		sb.append("    gitCommit: " + gitCommit + "\n");
+		sb.append("    gitBranch: " + gitBranch + "\n");
+		sb.append("    gitURL:    " + gitURL + "\n");
+		return sb.toString();
 	}
 }
